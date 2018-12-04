7046e821b58a039494fb3761be17996492ba25ff
rookjc <rookjc@miamioh.edu>
2018-12-04 13:06:12 -0500

    Finished DES Algorithm (Java)

--- DES_Encryption.java
+++ DES_Encryption.java
@@ -24,6 +24,7 @@ public class DES_Encryption {
 		String strMessage = "0123456789ABCDEF";
 		boolean[] messageDES = messageDES(strMessage, subKeys);
 		
+		displayBits(messageDES, 8);
 		
 //		System.out.print("process ints or chars? ");
 //		char choice = keyboard.next().charAt(0);
@@ -88,6 +89,60 @@ public class DES_Encryption {
 			20, 21, 22, 23, 24, 25, 
 			24, 25, 26, 27, 28, 29, 
 			28, 29, 30, 31, 32, 1};
+	
+	public static int[][] sBoxes = new int[][] {
+		
+		{14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7, 
+		0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8, 
+		4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0, 
+		15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13}, // S1
+
+		{15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10, 
+		3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5, 
+		0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15, 
+		13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9}, // S2
+
+		{10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8, 
+		13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1, 
+		13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7, 
+		1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12},  // S3
+
+		{7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15, 
+		13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9, 
+		10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4, 
+		3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14}, // S4
+
+		{2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9, 
+		14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6, 
+		4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14, 
+		11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3}, // S5
+
+		{12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11, 
+		10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8, 
+		9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6, 
+		4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13}, // S6
+
+		{4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1, 
+		13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6, 
+		1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2, 
+		6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12}, // S7
+
+		{13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7, 
+		1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2, 
+		7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8, 
+		2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11} // S8
+	};
+	
+	
+	public static int[] P = new int[] {16, 7, 20, 21, 
+			29, 12, 28, 17, 
+			1, 15, 23, 26, 
+			5, 18, 31, 10, 
+			2, 8, 24, 14, 
+			32, 27, 3, 9, 
+			19, 13, 30, 6, 
+			22, 11, 4, 25};
+	
 
 	// Given a hexadecmial key, generate the 16 subkeys needed for the encryption algorithm
 	public static boolean[][] generateSubKeys(String strKey) {
@@ -130,8 +185,8 @@ public class DES_Encryption {
 				else
 					result[i][j] = C[i+1][pc2Index];
 			}
-			System.out.println("K" + (i+1)); // display each subkey (temporarily, for verification)
-			displayBits(result[i], 6);
+			//System.out.println("K" + (i+1)); // display each subkey (temporarily, for verification)
+			//displayBits(result[i], 6);
 		}
 
 		return result;
@@ -245,8 +300,8 @@ public class DES_Encryption {
             }
             
             // permute the message
-            boolean[] permutedMessage = new boolean[56];
-            for (int i = 0; i < 56; i++) 
+            boolean[] permutedMessage = new boolean[64];
+            for (int i = 0; i < 64; i++) 
             {
                     permutedMessage[i] = message[initialpermute[i]-1];
             }
@@ -255,40 +310,109 @@ public class DES_Encryption {
             boolean[] left = new boolean[32];
             boolean[] right = new boolean[32];
             
-            for (int i = 0; i < message.length; i++)
+            for (int i = 0; i < permutedMessage.length; i++)
             {
                 if(i < 32)
-                    left[i] = message[i];
+                    left[i] = permutedMessage[i];
                 else
-                    right[i] = message[i+32];
+                    right[i-32] = permutedMessage[i];
             }
             
-            // make the expansion array: the right message half expands to 48 bits 
-            boolean[] expanded = new boolean[48];
-            for (int i = 0; i < 48; i++)
+            System.out.println(subkeys.length);
+            
+            //boolean[] fOutput = f(right, subkeys[0]);
+            
+            for (int i = 0; i < 16; i++) {
+            	boolean[] fOutput = f(right, subkeys[i]);
+            	boolean[] temp = xorBits(left, fOutput);
+            	left = right;
+            	right = temp;
+            }
+            
+            boolean[] R16L16 = new boolean[64];
+            for (int i = 0; i < 32; i++) {
+            	R16L16[i] = right[i];
+            	R16L16[i+32] = left[i];
+            }
+            
+            boolean[] encryptedValue = new boolean[64];
+            for (int i = 0; i < 64; i++) 
             {
-                expanded[i] = right[exPermute[i]-1];
-            }
-            // A note: I believe the following expansions are derived from the modified message from the prior round         
-            
-            
-            // XOR Operation between expanded right and corresponding round key
-            boolean[] xorOperated = new boolean[48];
-            for (int i = 0; i < 48; i++)
-            {
-                xorOperated[i] = expanded[i] ^ subkeys[0][i]; // can be changed to work for each iteration if needed
-                
+                    encryptedValue[i] = R16L16[finalpermute[i]-1];
             }
             
             
-            // Substitution (S) Boxes - 8 boxes: 6 bit input, 4 bit output
-            
-            
-            
-            
+            return encryptedValue;
             
 	} // end messageDES method
 	
+	public static boolean[] xorBits(boolean[] bits1, boolean[] bits2) {
+		 boolean[] xorOperated = new boolean[bits1.length];
+        for (int i = 0; i < xorOperated.length; i++)
+        {
+            xorOperated[i] = bits1[i] ^ bits2[i]; // can be changed to work for each iteration if needed
+            
+        }
+        
+        return xorOperated;
+	}
 	
+	public static boolean[] f(boolean[] R, boolean[] K) {
+		
+		// make the expansion array: the right message half expands to 48 bits 
+        boolean[] expanded = new boolean[48];
+        for (int i = 0; i < 48; i++)
+        {
+            expanded[i] = R[exPermute[i]-1];
+        }
+        // A note: I believe the following expansions are derived from the modified message from the prior round         
+        
+        
+        // XOR Operation between expanded right and corresponding round key
+        boolean[] xorOperated = xorBits(expanded, K);
+//        
+//        for (int i = 0; i < 48; i++)
+//        {
+//            xorOperated[i] = expanded[i] ^ K[i]; // can be changed to work for each iteration if needed
+//            
+//        }
+        
+        boolean[] concatedSValues = new boolean[32];
+        int concatedSValuesPosition = 0;
+        
+        for (int i = 0; i < 48; i += 6) {
+        	int index = sBoxIndex(new boolean[] {xorOperated[i], xorOperated[i+1], xorOperated[i+2],
+        			xorOperated[i+3], xorOperated[i+4], xorOperated[i+5]});
+        	
+        	int sVal = sBoxes[i/6][index];
+        	
+        	concatedSValues[concatedSValuesPosition] = sVal >= 8;
+        	concatedSValues[concatedSValuesPosition + 1] = (sVal % 8) >= 4;
+        	concatedSValues[concatedSValuesPosition + 2] = (sVal % 4) >= 2;
+        	concatedSValues[concatedSValuesPosition + 3] = (sVal % 2) >= 1;
+        	
+        	concatedSValuesPosition += 4;
+        	
+        }
+        
+        // Permutation P, the last step of the f function
+        
+        boolean[] result = new boolean[32];
+        for (int i = 0; i < 32; i++) 
+        {
+                result[i] = concatedSValues[P[i]-1];
+        }
+        
+		return result; // result from f function
+	}
 
+	public static int sBoxIndex(boolean[] B) {
+		return (B[0] ? 32 : 0)
+				+ (B[1] ? 8 : 0)
+				+ (B[2] ? 4 : 0)
+				+ (B[3] ? 2 : 0)
+				+ (B[4] ? 1 : 0)
+				+ (B[5] ? 16 : 0);
+	}
+	
 }
