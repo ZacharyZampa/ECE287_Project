85742d23992557d396e50a445f7acf969462495a
Zachary Zampa <zackzampa@gmail.com>
2018-12-06 12:00:51 -0500

    Added subkey generation
    Some bugs exist with calling values from key and sometimes 0s show up when entering input values

--- DES_Encryption_Hardware/DES_Encrypter.v
+++ DES_Encryption_Hardware/DES_Encrypter.v
@@ -3,6 +3,15 @@ input[63:0] inValues, key;
 output[63:0] outValues;
 input decrypt; // 1'b1 if the module should decrypt, rather than encrypt
 
-assign outValues = key; // temporary, for testing
+Subkey_Generator subkgen(key, subkey0, subkey1, subkey2,subkey3,subkey4,subkey5,subkey6,subkey7,
+			subkey8, subkey9, subkey10, subkey11, subkey12, subkey13, subkey14, subkey15, keyCheck);
+			
+wire [47:0]subkey0, subkey1, subkey2,subkey3,subkey4,subkey5,subkey6,subkey7,
+			subkey8, subkey9, subkey10, subkey11, subkey12, subkey13, subkey14, subkey15;
+wire [63:0]keyCheck;
+			
+			
+			
+assign outValues = keyCheck; // temporary, for testing
 
 endmodule
